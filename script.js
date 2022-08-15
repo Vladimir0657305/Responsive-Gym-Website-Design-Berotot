147da6dd8e9f30282c493632bb44411237b15d44
Vladimir0657305 <vv0657305@gmail.com>
2022-08-15 22:12:44 +0300

    ending code

--- script.js
+++ script.js
@@ -40,6 +40,49 @@ const scrollHeader = () => {
 }
 window.addEventListener('scroll', scrollHeader)
 
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+sr.reveal(`.home__data, .footer__container, .footer__group`);
+sr.reveal(`.home__img`, { delay: 700, origin: 'bottom' });
+sr.reveal(`.logos__img, .program__card, .pricing__card`, { interval: 100 });
+sr.reveal(`.choose_img, .calculate__content`, { origin: 'left' });
+sr.reveal(`.choose_content, calculate__img`, { origin: 'right' });
+
 /*=============== CALCULATE JS ===============*/
 const calculateForm = document.getElementById('calculate-form'),
     calculateCm = document.getElementById('calculate-cm'),
@@ -86,3 +129,38 @@ const calculateBmi = (e) => {
 }
 
 calculateForm.addEventListener('submit', calculateBmi)
+
+/*=============== EMAIL JS ==================*/
+
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message'),
+    contactUser = document.getElementById('contact-user');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    // Check if the field  has  a value
+    if (contactUser.value === '') {
+        contactMessage.classList.remove('color-green');
+        contactMessage.classList.add('color-red');
+        contactMessage.textContent = 'Your must enter your email 🤔';
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 3000)
+    } else {
+        // serviceID - templateID -#form - publicKey
+        emailjs.sendForm('service_8ii265v', 'template_guf0dsp', '#contact-form', 'VQaq6RG_bXgUd7BNm')
+            .then(() => {
+                contactMessage.classList.add('color-green');
+                contactMessage.textContent = 'You registered successfully 👍';
+                setTimeout(() => {
+                    contactMessage.textContent = ''
+                }, 3000)
+            }, (error) => {
+                alert('OOPS! SOMETHINGS HAS FAILED...', error)
+            })
+        contactUser.value = '';
+    }
+}
+
+contactForm.addEventListener('submit', sendEmail)
